a1ce4bdc0905fedd1b03fb0d3bf3c021a266f8f3
enzhe <144530797+asholenzhe@users.noreply.github.com>
2025-01-17 17:11:29 +0300

    third homework js

---
+++ classes/Company.js
@@ -0,0 +1,9 @@
+export class Company {
+    constructor(id, name, created, country, products){
+        this.id = id;
+        this.name = name;
+        this.created = created;
+        this.country = country;
+        this.products = products;
+    }
+}
---
+++ classes/Product.js
@@ -0,0 +1,9 @@
+export class Product {
+    constructor(id, name, description, company, reviews) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.company = company;
+        this.reviews = reviews;
+    }
+}
